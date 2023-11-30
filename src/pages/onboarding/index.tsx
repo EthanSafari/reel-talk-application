f3c8d5eb906fb99490753ee34ba05481f11c51f2
ethan <eman5797@gmail.com>
2023-11-29 23:38:13 -0500

    adding basic onboarding page

---
+++ src/pages/onboarding/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC, useState, ChangeEvent } from "react";
+
+const Onboarding: FC = () => {
+    return (
+        <div>
+            Hello world
+        </div>
+    )
+};
+
+export default Onboarding;
