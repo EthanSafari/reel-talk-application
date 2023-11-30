3f627a8a9c11935c805d4214d239668a58a0b8bf
ethan <eman5797@gmail.com>
2023-11-30 02:36:09 -0500

    starting search

---
+++ src/components/searchbar.tsx
@@ -0,0 +1,15 @@
+import React, { FC, useState, ChangeEvent } from "react";
+
+interface SearchBarProps {
+    forOnboarding: boolean
+};
+
+const SearchBar: FC<SearchBarProps> = ({ forOnboarding }) => {
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default SearchBar;
