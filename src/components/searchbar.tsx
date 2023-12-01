1b7a6fd8aa7cb8e486bcc8bcbe70cbc4d1808f50
ethan <eman5797@gmail.com>
2023-11-30 23:52:10 -0500

    added fontawesome

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,8 +1,5 @@
 import { Genre } from "@/pages/onboarding/genre-selection";
 import React, { FC, useState, ChangeEvent } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-
 
 interface SearchBarProps {
     forOnboarding: boolean,
@@ -11,12 +8,12 @@ interface SearchBarProps {
 
 const SearchBar: FC<SearchBarProps> = ({ forOnboarding, genres }) => {
     return (
-        <div className="flex w-1/3 justify-center bg-[#2D2D2D] items-center">
-
+        <div className="flex w-1/3 justify-center items-center">
+            <i className={`fa-solid fa-magnifying-glass ${forOnboarding ? 'bg-[#2D2D2D]' : 'bg-[#EBEBEB]'} h-7 flex justify-center items-center w-7`}></i>
             <input
                 type="text"
                 placeholder="Search"
-                className={`w-4/5 h-2/4 ${forOnboarding ? 'bg-[#2D2D2D]' : 'bg-[#EBEBEB]'}`}
+                className={`w-4/5 h-7 ${forOnboarding ? 'bg-[#2D2D2D]' : 'bg-[#EBEBEB]'}`}
             />
         </div>
     )
